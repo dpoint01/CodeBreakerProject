bf41d92f3fad6842514e93ceb96a6c7eccec01d9
David Pointeau <dpointeau@everquote.com>
2017-03-03 16:19:03 -0500

    Add setHiddenFields

--- src/assets/main.js
+++ src/assets/main.js
@@ -7,3 +7,18 @@ function guess() {
 }
 
 //implement new functions here
+function setHiddenFields() {
+  var min = Math.ceil(0),
+      max = Math.floor(9999);
+
+  answer.value = Math.floor(Math.random() * (max - min)) + min;
+  makeFourCharactersLong();
+}
+
+function makeFourCharactersLong() {
+  while (answer.value.toString().length < 4) {
+    answer.value.toString() = '0' + answer.value.toString();
+  }
+
+  answer.value = parseInt(answer.value.toString());
+}
